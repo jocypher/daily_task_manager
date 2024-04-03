324c4697206e20ce45f5a772400a2da2c5c66841
willyjo1 <arthurwilchield@gmail.com>
2024-04-03 12:05:38 +0000

    update: core architecture setup made

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const backgroundColor = Color.fromARGB(161, 7, 4, 120);
+  static const headingTextColor = Color.fromARGB(255, 51, 153, 221);
+  static const primaryTextColor = Color(0xfffffffff);
+  static const secondaryTextColor = Color.fromARGB(111, 255, 255, 255);
+  static const boardColor1 = Color.fromARGB(255, 255, 255, 255);
+  static const boardColor2 = Color.fromARGB(255, 251, 233, 69);
+  static const boardColor3 = Color.fromARGB(255, 110, 231, 255);
+}
