3dd1b91ce19573120c8cc9aed5c2b5c21ca02269
willyjo1 <arthurwilchield@gmail.com>
2024-04-03 17:43:23 +0000

    update:removed an unused import

--- lib/module/home/presentation/screen/board_section.dart
+++ lib/module/home/presentation/screen/board_section.dart
@@ -1,6 +1,5 @@
 import 'package:daily_task_manager/module/home/presentation/screen/home_controller.dart';
 import 'package:daily_task_manager/module/home/presentation/widget/board_widget.dart';
-import 'package:daily_task_manager/module/home/presentation/widget/task_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
