1d17f3c1f1ebe4718aff37447edbe3cf7ce6835d
willyjo1 <arthurwilchield@gmail.com>
2024-04-03 13:07:18 +0000

    update:home greeting and heading designed

--- lib/module/home/presentation/screen/home_screen.dart
+++ lib/module/home/presentation/screen/home_screen.dart
@@ -11,8 +11,128 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.backgroundColor,
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 60),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(Icons.person),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(
+                        height: 30,
+                        width: 30,
+                        child: Stack(
+                          fit: StackFit.loose,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: AppColors.boardColor2,
+                                  borderRadius: BorderRadius.circular(100)),
+                              child: const Center(
+                                child: Icon(
+                                  Icons.notifications_outlined,
+                                  color: AppColors.primaryTextColor,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 20,
+                              right: 4,
+                              child: Container(
+                                width: 20,
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(25),
+                                    color: AppColors.headingTextColor),
+                                child: const Center(
+                                  child: Text(
+                                    "12",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 10),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 1,
+                      ),
+                      Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                            color: AppColors.boardColor2,
+                            borderRadius: BorderRadius.circular(100)),
+                        child: const Center(
+                          child: Icon(
+                            Icons.add,
+                            color: AppColors.primaryTextColor,
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 100),
+              child: Text("Good\nMorning",
+                  softWrap: true,
+                  style: TextStyle(
+                      fontSize: 70,
+                      color: AppColors.boardColor2,
+                      inherit: false,
+                      wordSpacing: -5.0,
+                      letterSpacing: 2.0)),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Today's Monday",
+                      style: TextStyle(color: AppColors.primaryTextColor),
+                    ),
+                    Text(
+                      "Dec 12, 2002",
+                      style: TextStyle(color: AppColors.secondaryTextColor),
+                    )
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "75% Done",
+                      style: TextStyle(color: AppColors.primaryTextColor),
+                    ),
+                    Text(
+                      "Completed Tasks",
+                      style: TextStyle(color: AppColors.secondaryTextColor),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
